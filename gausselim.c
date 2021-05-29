f2b8aaef27a7111823874bdeb51a0a12a55b7480
Rabindra Harlalka <rabindra-harlalka@users.noreply.github.com>
2021-05-29 21:10:18 +0530

    gausselim: handle different scenarios in substitution

--- gausselim.c
+++ gausselim.c
@@ -9,6 +9,7 @@ recipe:
 */
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -17,121 +18,125 @@ recipe:
 #include <math.h>
 #endif
 
-int gauss_elim(int m, int n, double A[][n], double* x, bool do_partial_pivoting);
-int eliminate(int m, int n, double A[][n], bool do_partial_pivoting);
+int gauss_elim(int m, int n, double A[][n], double* x, bool do_partial_pivoting, bool augmented_matrix);
+int eliminate(int m, int n, double A[][n], bool do_partial_pivoting, bool augmented_matrix);
 int substitute(int m, int n, double A[][n], double* x);
 
-static int find_row_index_with_max_pivot(int m, int n, double A[][n], int i, int p);
+static int find_row_index_with_max_pivot(int m, int n, double A[][n], int i, int k);
 static void row_swap(int n, double A[][n], int i, int j);
 static void swap_element(int n, double A[][n], int i, int j, int c);
+static bool row_is_all_zeros(int n, double A[][n], int row, int last_index);
 
-static void debug_matrix(int m, int n, double A[][n], int pivotIndex, const char* heading);
+static void debug_matrix_with_pivot_info(int m, int n, double A[][n], int pivot_index, const char* heading);
+static void debug_matrix(int m, int n, double A[][n]);
 static void print_matrix(int m, int n, double A[][n]);
+static void debug_message(const char* message, ...);
+static void print_error(const char* message, ...);
 
 #define EPSILON 0.0001
 #ifdef TEST
 void test_1()
 {
-	double matrix_A[4][5] = {
-		{ 0, 2, 0, 1, 0 },
-		{ 2, 2, 3, 2, -2 },
-		{ 4, -3, 0, 1, -7 },
-		{ 6, 1, -6, -5, 6 }
-	};
+    double matrix_A[4][5] = {
+        { 0, 2, 0, 1, 0 },
+        { 2, 2, 3, 2, -2 },
+        { 4, -3, 0, 1, -7 },
+        { 6, 1, -6, -5, 6 }
+    };
 
-	double x[4];
-	int ret = gauss_elim(4, 5, matrix_A, x, true);
+    double x[4];
+    int ret = gauss_elim(4, 5, matrix_A, x, true, true);
 
-	assert(ret == 0);
-	assert(fabs(x[0] - -0.5) <= EPSILON);
-	assert(fabs(x[1] - 1.0) <= EPSILON);
-	assert(fabs(x[2] - 0.3333) <= EPSILON);
-	assert(fabs(x[3] - -2.0) <= EPSILON);
+    assert(ret == 0);
+    assert(fabs(x[0] - -0.5) <= EPSILON);
+    assert(fabs(x[1] - 1.0) <= EPSILON);
+    assert(fabs(x[2] - 0.3333) <= EPSILON);
+    assert(fabs(x[3] - -2.0) <= EPSILON);
 
-	/* without partial pivoting */
-	double matrix_AA[4][5] = {
-		{ 0, 2, 0, 1, 0 },
-		{ 2, 2, 3, 2, -2 },
-		{ 4, -3, 0, 1, -7 },
-		{ 6, 1, -6, -5, 6 }
-	};
+    /* without partial pivoting */
+    double matrix_AA[4][5] = {
+        { 0, 2, 0, 1, 0 },
+        { 2, 2, 3, 2, -2 },
+        { 4, -3, 0, 1, -7 },
+        { 6, 1, -6, -5, 6 }
+    };
 
-	ret = gauss_elim(4, 5, matrix_AA, x, false);
+    ret = gauss_elim(4, 5, matrix_AA, x, false, true);
 
-	assert(ret == 0);
-	assert(fabs(x[0] - -0.5) <= EPSILON);
-	assert(isnan(x[1]));
-	assert(isnan(x[2]));
-	assert(isnan(x[3]));
+    assert(ret == 0);
+    assert(fabs(x[0] - -0.5) <= EPSILON);
+    assert(isnan(x[1]));
+    assert(isnan(x[2]));
+    assert(isnan(x[3]));
 }
 
 void test_2()
 {
-	double matrix_A[5][6] = {
-		{ 13, 4, 5, 0, 9, 101 },
-		{ 33, -2, -7, 8, 0, 3 },
-		{ 23, 32, 9, 5, 1, 7 },
-		{ 54, 34, 87, 2, 4, 66 },
-		{ -5, 6, 7, 8, 9, 10 }
-	};
+    double matrix_A[5][6] = {
+        { 13, 4, 5, 0, 9, 101 },
+        { 33, -2, -7, 8, 0, 3 },
+        { 23, 32, 9, 5, 1, 7 },
+        { 54, 34, 87, 2, 4, 66 },
+        { -5, 6, 7, 8, 9, 10 }
+    };
 
-	double x[5];
-	int ret = gauss_elim(5, 6, matrix_A, x, true);
+    double x[5];
+    int ret = gauss_elim(5, 6, matrix_A, x, true, true);
 
-	assert(ret == 0);
-	assert(fabs(x[0] - 1.7424) <= EPSILON);
-	assert(fabs(x[1] - -0.0149) <= EPSILON);
-	assert(fabs(x[2] - -0.5640) <= EPSILON);
-	assert(fabs(x[3] - -7.3098) <= EPSILON);
-	assert(fabs(x[4] - 9.0253) <= EPSILON);
+    assert(ret == 0);
+    assert(fabs(x[0] - 1.7424) <= EPSILON);
+    assert(fabs(x[1] - -0.0149) <= EPSILON);
+    assert(fabs(x[2] - -0.5640) <= EPSILON);
+    assert(fabs(x[3] - -7.3098) <= EPSILON);
+    assert(fabs(x[4] - 9.0253) <= EPSILON);
 
-	/* without partial pivoting */
-	double matrix_AA[5][6] = {
-		{ 13, 4, 5, 0, 9, 101 },
-		{ 33, -2, -7, 8, 0, 3 },
-		{ 23, 32, 9, 5, 1, 7 },
-		{ 54, 34, 87, 2, 4, 66 },
-		{ -5, 6, 7, 8, 9, 10 }
-	};
-	ret = gauss_elim(5, 6, matrix_AA, x, false);
+    /* without partial pivoting */
+    double matrix_AA[5][6] = {
+        { 13, 4, 5, 0, 9, 101 },
+        { 33, -2, -7, 8, 0, 3 },
+        { 23, 32, 9, 5, 1, 7 },
+        { 54, 34, 87, 2, 4, 66 },
+        { -5, 6, 7, 8, 9, 10 }
+    };
+    ret = gauss_elim(5, 6, matrix_AA, x, false, true);
 
-	assert(ret == 0);
-	assert(fabs(x[0] - 1.7424) <= EPSILON);
-	assert(fabs(x[1] - -0.0149) <= EPSILON);
-	assert(fabs(x[2] - -0.5640) <= EPSILON);
-	assert(fabs(x[3] - -7.3098) <= EPSILON);
-	assert(fabs(x[4] - 9.0253) <= EPSILON);
+    assert(ret == 0);
+    assert(fabs(x[0] - 1.7424) <= EPSILON);
+    assert(fabs(x[1] - -0.0149) <= EPSILON);
+    assert(fabs(x[2] - -0.5640) <= EPSILON);
+    assert(fabs(x[3] - -7.3098) <= EPSILON);
+    assert(fabs(x[4] - 9.0253) <= EPSILON);
 
 }
 
 void test_3()
 {
-	double matrix_A[][4] = {
-		{ 1, 0, -5, 0 },	// father 5 times older than child
-		{ 1, 1, 0, 99 },	// sum of parents' age is 99
-		{ 0, 1, -3.5, 0 }	// mother 3.5 times older than child
-	};
+    double matrix_A[][4] = {
+        { 1, 0, -5, 0 },    // father 5 times older than child
+        { 1, 1, 0, 99 },    // sum of parents' age is 99
+        { 0, 1, -3.5, 0 }   // mother 3.5 times older than child
+    };
 
-	double x[3];
-	int ret = gauss_elim(3, 4, matrix_A, x, true);
+    double x[3];
+    int ret = gauss_elim(3, 4, matrix_A, x, true, true);
 
-	assert(ret == 0);
-	assert(fabs(x[0] - 58.2353) <= EPSILON);
-	assert(fabs(x[1] - 40.7647) <= EPSILON);
-	assert(fabs(x[2] - 11.6471) <= EPSILON);
+    assert(ret == 0);
+    assert(fabs(x[0] - 58.2353) <= EPSILON);
+    assert(fabs(x[1] - 40.7647) <= EPSILON);
+    assert(fabs(x[2] - 11.6471) <= EPSILON);
 
-	/* without partial pivoting */
-	double matrix_AA[][4] = {
-		{ 1, 0, -5, 0 },	// father 5 times older than child
-		{ 1, 1, 0, 99 },	// sum of parents' age is 99
-		{ 0, 1, -3.5, 0 }	// mother 3.5 times older than child
-	};
-	ret = gauss_elim(3, 4, matrix_AA, x, false);
+    /* without partial pivoting */
+    double matrix_AA[][4] = {
+        { 1, 0, -5, 0 },    // father 5 times older than child
+        { 1, 1, 0, 99 },    // sum of parents' age is 99
+        { 0, 1, -3.5, 0 }   // mother 3.5 times older than child
+    };
+    ret = gauss_elim(3, 4, matrix_AA, x, false, true);
 
-	assert(ret == 0);
-	assert(fabs(x[0] - 58.2353) <= EPSILON);
-	assert(fabs(x[1] - 40.7647) <= EPSILON);
-	assert(fabs(x[2] - 11.6471) <= EPSILON);
+    assert(ret == 0);
+    assert(fabs(x[0] - 58.2353) <= EPSILON);
+    assert(fabs(x[1] - 40.7647) <= EPSILON);
+    assert(fabs(x[2] - 11.6471) <= EPSILON);
 
 }
 #endif
@@ -139,198 +144,226 @@ void test_3()
 #ifndef NOMAIN
 int main(int argc, char** argv)
 {
-	printf("******* Gaussian Elimination Program ******\n");
+    printf("******* Gaussian Elimination Program ******\n");
 
 #ifdef TEST
-	printf("Running tests\n");
-	test_1();
-	test_2();
-	test_3();
-	printf("Finished running tests\n");
+    printf("Running tests\n");
+    test_1();
+    test_2();
+    test_3();
+    printf("Finished running tests\n");
+
+    return 0;
 #else
-	/** input: parse command line arguments **/
-	if (argc < 2)
-	{
-		printf("ERROR: Provide an augmented matrix as input\n");
-		return -1;
-	}
-	bool do_partial_pivot = true;
-	int arg_i = 1;
-	while (strncmp(argv[arg_i], "--", 2) == 0)
-	{
-		if (strcmp(argv[arg_i], "--no-pivot") == 0)
-		{
-			do_partial_pivot = false;
-		}
-		else
-		{
-			printf("WARNING: unrecognized option %s\n", argv[arg_i]);
-		}
-		arg_i++;
-	}
+    /** input: parse command line arguments **/
+    if (argc < 2)
+    {
+        printf("ERROR: Provide a matrix as input\n");
+        return -1;
+    }
+    bool do_partial_pivot = true;
+    bool augmented_matrix = true;
+    int arg_i = 1;
+    while (strncmp(argv[arg_i], "--", 2) == 0)
+    {
+        if (strcmp(argv[arg_i], "--no-pivot") == 0)
+        {
+            do_partial_pivot = false;
+        }
+        else if (strcmp(argv[arg_i], "--no-aug") == 0)
+        {
+            augmented_matrix = false;
+        }
+        else if (strlen(argv[arg_i]) != 2)
+        {
+            printf("WARNING: unrecognized option %s\n", argv[arg_i]);
+        }
+        arg_i++;
+    }
 
-	int n = atoi(argv[arg_i]);
-	int elements_start = arg_i + 1;
-	int m = (argc - elements_start) / n;
-	if (n < 1 || m < 1)
-	{
-		printf("ERROR: The input matrix must have at least one row and column\n");
-		return -1;
-	}
-	if (m < n - 1)
-	{
-		// elimination will work, but backward substiution won't
-		printf("ERROR: The input augmented matrix must have at least as many rows as"
-				" columns in the co-efficient matrix\n");
-		return -1;
-	}
-	printf("Number of columns = %d, Number of rows = %d\n", n, m);
-	double A[m][n];
-	for (int i = elements_start; i < argc; i++)
-	{
-		int col = (i - elements_start) % n;
-		int row = (i - elements_start) / n;
-		A[row][col] = atof(argv[i]);
-	}
-	print_matrix(m, n, A);
+    int n = atoi(argv[arg_i]);
+    int elements_start = arg_i + 1;
+    int m = (argc - elements_start) / n;
+    if (n < 1 || m < 1)
+    {
+        printf("ERROR: The input matrix must have at least one row and column\n");
+        return -1;
+    }
+    printf("Number of columns = %d, Number of rows = %d\n", n, m);
+    double A[m][n];
+    for (int i = elements_start; i < argc; i++)
+    {
+        int col = (i - elements_start) % n;
+        int row = (i - elements_start) / n;
+        A[row][col] = atof(argv[i]);
+    }
 
-	/** run **/
-	double x[n - 1];
-	int ret = gauss_elim(m, n, A, x, do_partial_pivot);
-	if (ret == 0)
-	{
-		for (int i = 0; i < sizeof(x)/sizeof(double); i++)
-		{
-			printf("x[%d] = %8.4f\n", i, x[i]);
-		}
-	}
+    /** run **/
+    double x[n - 1];
+    int ret = gauss_elim(m, n, A, x, do_partial_pivot, augmented_matrix);
+    if (ret == 0)
+    {
+        printf("\nOutput\n");
+        if (augmented_matrix)
+        {
+            for (int i = 0; i < sizeof(x)/sizeof(double); i++)
+            {
+                printf("x[%d] = %8.4f\n", i, x[i]);
+            }
+        }
+        else
+        {
+            print_matrix(m, n, A);
+        }
+    }
+
+    return ret;
 #endif
-
-	return 0;
 }
 #endif	// NOMAIN
 
 /**
- * Given augmented matrix A of size mxn, performs Gaussian Elimination
+ * Given a matrix A of size mxn, performs Gaussian Elimination
  * by doing forward elimination and backward substituion, and puts the
  * result in the given array x.
  * Partial pivoting is performed to avoid pitfalls, if opted.
  */
-int gauss_elim(int m, int n, double A[][n], double* x, bool do_partial_pivoting)
+int gauss_elim(int m, int n, double A[][n], double* x, bool do_partial_pivoting, bool augmented_matrix)
 {
-	/** Dump for debugging **/
-	print_matrix(m, n, A);
+    /** Dump for debugging **/
+    debug_matrix(m, n, A);
 
-	/** forward elimination **/
-	int ret = eliminate(m, n, A, do_partial_pivoting);
+    /** forward elimination **/
+    int ret = eliminate(m, n, A, do_partial_pivoting, augmented_matrix);
 
-	/** back substitution **/
-	ret &= substitute(m, n, A, x);
+    /** back substitution **/
+    if (augmented_matrix) ret |= substitute(m, n, A, x);
 
-	return ret;
+    return ret;
 }
 
-int eliminate(int m, int n, double A[][n], bool do_partial_pivoting)
+int eliminate(int m, int n, double A[][n], bool do_partial_pivoting, bool augmented_matrix)
 {
-	for (int p = 0; p < m - 1; p++)
-	{
-		// 1. check if partial pivoting is needed
-		double pivot = A[p][p];
-		// a. divide by zero
-		if (do_partial_pivoting == true && pivot == 0)
-		{
-			// interchange A[i] with A[r] where A[r][p] is max
-			int r = find_row_index_with_max_pivot(m, n, A, p, p);
-			row_swap(n, A, p, r);
-			pivot = A[p][p];	// update the pivot because we have swapped
+    int numCoeffCols = augmented_matrix ? n - 1 : n;
+    int numPivots = (m > numCoeffCols) ? numCoeffCols : (m - 1);
+    for (int k = 0; k < numPivots; k++)
+    {
+        // 1. check if partial pivoting is needed
+        double pivot = A[k][k];
+        // a. divide by zero
+        if (do_partial_pivoting == true && pivot == 0)
+        {
+            // interchange A[i] with A[r] where A[r][k] is max
+            int r = find_row_index_with_max_pivot(m, n, A, k, k);
+            row_swap(n, A, k, r);
+            pivot = A[k][k];    // update the pivot because we have swapped
 
-			debug_matrix(m, n, A, p, "div0");
-		}
+            debug_matrix_with_pivot_info(m, n, A, k, "div0");
+        }
 
-		for (int k = p + 1; k < m; k++)	// for all rows below the pivot row Rp
-		{
-			// b. round-off error
-			if (do_partial_pivoting == true && abs(pivot) < abs(A[k][p]))
-			{
-				row_swap(n, A, p, k);	
-				pivot = A[p][p];	// update the pivot because we have swapped
+        for (int i = k + 1; i < m; i++) // for all rows below the pivot row Rp
+        {
+            // b. round-off error
+            if (do_partial_pivoting == true && abs(pivot) < abs(A[i][k]))
+            {
+                row_swap(n, A, k, i);
+                pivot = A[k][k];    // update the pivot because we have swapped
 
-				debug_matrix(m, n, A, p, "round-off");
-			}
+                debug_matrix_with_pivot_info(m, n, A, k, "round-off");
+            }
 
-			if (A[k][p] == 0) continue;
+            if (A[i][k] == 0) continue;
 
-			// 2. eliminate
-			double multiplier = A[k][p] / pivot;			// division operation
+            // 2. eliminate
+            double multiplier = A[i][k] / pivot;    // division operation
 
-			A[k][p] = 0;	// set A[i][p] to zero directly
-			for (int j = p + 1; j < n; j++)	// rest of the columns
-			{
-				A[k][j] -= A[p][j] * multiplier;	// subtraction and multiplication operations
-			}
-		}
+            A[i][k] = 0;    // set A[i][k] to zero directly
+            for (int j = k + 1; j < n; j++) // rest of the columns
+            {
+                A[i][j] -= A[k][j] * multiplier;    // subtraction and multiplication operations
+            }
+        }
 
-		debug_matrix(m, n, A, p, "elim");
-	}
+        debug_matrix_with_pivot_info(m, n, A, k, "elim");
+    }
 
-	return 0;
+    return 0;
 }
 
 int substitute(int m, int n, double A[][n], double* x)
 {
-	bool x_solved[n];
-	memset(x_solved, false, n);
+    bool x_solved[n];
+    memset(x_solved, false, n);
 
-	for (int k = m - 1; k >= 0; k--)
-	{
-		char unsolved_index = -1;
-		// verify that the row is solved
-		double b = A[k][n - 1];
-		for (int j = 0; j < n - 1; j++)
-		{
-			if (A[k][j] != 0)
-			{
-				if (x_solved[j] == false)
-				{
-					// ERROR: solution doesn't exist, or not enough equations given!
-					if (unsolved_index >= 0) return -1;
-					unsolved_index = j;
-				}
-				else
-				{
-					b -= A[k][j] * x[j];
-				}
-			}
-		}
-		if (unsolved_index >= 0)
-		{
-			x[unsolved_index] = b / A[k][unsolved_index];
-			x_solved[unsolved_index] = true;
-		}
-	}
+    for (int i = m - 1; i >= 0; i--)
+    {
+        double b = A[i][n - 1];
+        if (row_is_all_zeros(n, A, i, n - 1))
+        {
+            if ((int)b == 0)
+            {
+                debug_message("Skipping row %d as it's all zeros\n", i);
+            }
+            else
+            {
+                print_error("ERROR: non-zero b %8.4f for zero row %d.. inconsistent system.\n", b, i);
+                return -2;
+            }
+        }
 
-	return 0;
+        char unsolved_index = -1;
+        // verify that the row is solved
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (A[i][j] != 0)
+            {
+                if (x_solved[j] == false)
+                {
+                    if (unsolved_index >= 0)
+                    {
+                        debug_message("Free variable %d found.. infinite solutions exist. Picking value 1\n", j);
+                        b -= A[i][j];
+                        x[j] = 1;
+                        x_solved[j] = true;
+                        continue;
+                    }
+                    unsolved_index = j;
+                }
+                else
+                {
+                    b -= A[i][j] * x[j];
+                }
+            }
+        }
+        if (unsolved_index >= 0)
+        {
+            x[unsolved_index] = b / A[i][unsolved_index];
+            x_solved[unsolved_index] = true;
+        }
+    }
+
+    return 0;
 }
 
 /**
- * Finds the max element in the given column p from rows i to m, and returns the row index
+ * Finds the max element in the given column k from rows i to m, and returns the row index
  * having the max element.
  */
-static int find_row_index_with_max_pivot(int m, int n, double A[][n], int i, int p)
+static int find_row_index_with_max_pivot(int m, int n, double A[][n], int i, int k)
 {
-	int maxIndex = i;
-	double max = A[i][p];
+    int maxIndex = i;
+    double max = A[i][k];
 
-	for (i = i + 1; i < m; i++)
-	{
-		if (A[i][p] > max)
-		{
-			max = A[i][p];
-			maxIndex = i;
-		}
-	}
+    for (i = i + 1; i < m; i++)
+    {
+        if (A[i][k] > max)
+        {
+            max = A[i][k];
+            maxIndex = i;
+        }
+    }
 
-	return maxIndex;
+    return maxIndex;
 }
 
 /**
@@ -338,39 +371,76 @@ static int find_row_index_with_max_pivot(int m, int n, double A[][n], int i, int
  */
 static void row_swap(int n, double A[][n], int i, int j)
 {
-	for (int col = 0; col < n; col++)
-	{
-		swap_element(n, A, i, j, col);
-	}
+    for (int col = 0; col < n; col++)
+    {
+        swap_element(n, A, i, j, col);
+    }
 }
 
 static void swap_element(int n, double A[][n], int i, int j, int c)
 {
-	A[i][c] += A[j][c];
-	A[j][c] = A[i][c] - A[j][c];
-	A[i][c] = A[i][c] - A[j][c];
+    A[i][c] += A[j][c];
+    A[j][c] = A[i][c] - A[j][c];
+    A[i][c] = A[i][c] - A[j][c];
 }
 
-static void debug_matrix(int m, int n, double A[][n], int pivotIndex, const char* heading)
+static bool row_is_all_zeros(int n, double A[][n], int row, int last_index)
+{
+    bool zeros = true;
+    for (int j = 0; j < last_index; j++)
+    {
+        if (A[row][j] != 0)
+        {
+            zeros = false;
+            break;
+        }
+    }
+    return zeros;
+}
+
+static void debug_matrix_with_pivot_info(int m, int n, double A[][n], int pivot_index, const char* heading)
 {
 #ifdef PRINTDEBUG
-	printf("p = %d\n", pivotIndex);
-	printf("-------%s---------\n", heading);
-	print_matrix(m, n, A);
+    printf("k = %d\n", pivot_index);
+    printf("-------%s---------\n", heading);
+    print_matrix(m, n, A);
+#endif
+}
+
+static void debug_matrix(int m, int n, double A[][n])
+{
+#ifdef PRINTDEBUG
+    print_matrix(m, n, A);
 #endif
 }
 
 void print_matrix(int m, int n, double A[][n])
 {
+    for (int i = 0; i < m; i++)
+    {
+        printf("[%d]: ", i);
+        for (int j = 0; j < n; j++)
+        {
+            printf("%8.4f ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+static void debug_message(const char* message, ...)
+{
 #ifdef PRINTDEBUG
-	for (int i = 0; i < m; i++)
-	{
-		printf("[%d]: ", i);
-		for (int j = 0; j < n; j++)
-		{
-			printf("%8.4f ", A[i][j]);
-		}
-		printf("\n");
-	}
+    va_list args;
+    va_start(args, message);
+    vprintf(message, args);
+    va_end(args);
 #endif
+}
+
+static void print_error(const char* message, ...)
+{
+    va_list args;
+    va_start(args, message);
+    vfprintf(stderr, message, args);
+    va_end(args);
 }
--- measuregausselim.c
+++ measuregausselim.c
@@ -11,53 +11,53 @@ recipe:
 #include <stdbool.h>
 #include <sys/time.h>
 
-extern int eliminate(int m, int n, double A[][n], bool do_partial_pivoting);
+extern int eliminate(int m, int n, double A[][n], bool do_partial_pivoting, bool augmented_matrix);
 extern int substitute(int m, int n, double A[][n], double* x);
 
 long get_elapsed_us(struct timeval *begin)
 {
     struct timeval end;
-	gettimeofday(&end, 0);
-	long seconds = end.tv_sec - begin->tv_sec;
-	long microseconds = end.tv_usec - begin->tv_usec;
-	long elapsed = seconds * 1e6 + microseconds;
+    gettimeofday(&end, 0);
+    long seconds = end.tv_sec - begin->tv_sec;
+    long microseconds = end.tv_usec - begin->tv_usec;
+    long elapsed = seconds * 1e6 + microseconds;
 
-	return elapsed;
+    return elapsed;
 }
 
 int main()
 {
-	printf("n,elim,subs\n");
-	for (int n = 5; n <= 1000; n+= n < 100 ? 5 : 50)
-	{
-		// generate random agumented matrix A of size n
-		double A[n][n+1];
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j <= n; j++)
-			{
-				A[i][j] = rand() % 100;
-			}
-		}
-		// run Gaussian elimination and substitution on A and measure time taken for each
-		// step 1:
-		struct timeval begin;
-		gettimeofday(&begin, 0);
+    printf("n,elim,subs\n");
+    for (int n = 5; n <= 1000; n+= n < 100 ? 5 : 50)
+    {
+        // generate random agumented matrix A of size n
+        double A[n][n+1];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                A[i][j] = rand() % 100;
+            }
+        }
+        // run Gaussian elimination and substitution on A and measure time taken for each
+        // step 1:
+        struct timeval begin;
+        gettimeofday(&begin, 0);
 
-		eliminate(n, n, A, true);
+        eliminate(n, n, A, true, true);
 
-		long time_step1 = get_elapsed_us(&begin);
+        long time_step1 = get_elapsed_us(&begin);
 
-		// step 2:
-		double x[n];
+        // step 2:
+        double x[n];
 
-		gettimeofday(&begin, 0);
-		substitute(n, n, A, x);
+        gettimeofday(&begin, 0);
+        substitute(n, n, A, x);
 
-		long time_step2 = get_elapsed_us(&begin);
+        long time_step2 = get_elapsed_us(&begin);
 
-		printf("%d,%ld,%ld\n", n, time_step1, time_step2);
-	}
+        printf("%d,%ld,%ld\n", n, time_step1, time_step2);
+    }
 
-	return 0;
+    return 0;
 }
